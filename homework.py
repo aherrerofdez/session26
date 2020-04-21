a606ee0066424aaf2f6484c90b0a29502aa3bd87
zalolearn <zalolearn@gmail.com>
2020-04-21 14:14:00 +0200

    Homework Animation

--- homework.py
+++ homework.py
@@ -1,5 +1,6 @@
 from matplotlib import pylab as plt
 import pandas as pd
+import datetime
 
 # Read csv stock data from American Airlines (AA) and Delta Airlines (DA)
 df_AA = pd.read_csv("AAL.csv")
@@ -17,10 +18,15 @@ print(df_DA.tail(3))
 df_AA.Date = pd.to_datetime(df_AA.Date)
 df_DA.Date = pd.to_datetime(df_DA.Date)
 
+# Coronavirus Outbreak in the USA
+date_covid = datetime.datetime(2020, 2, 13)
+
 # Plotting
 plt.figure("American Airlines Stock vs Delta Airlines Stock")
 plt.plot(df_AA.Date, df_AA.Close, "b", label="American Airlines")
 plt.plot(df_DA.Date, df_DA.Close, "r", label="Delta Airlines")
+plt.plot(date_covid, df_AA.Close[df_AA.index[df_AA.Date == date_covid][0]], 'ko', label="Covid-19 Outbreak")
+plt.plot(date_covid, df_DA.Close[df_DA.index[df_DA.Date == date_covid][0]], 'ko')
 plt.legend(loc="upper left")
 
 plt.show()
--- homework_animation.py
+++ homework_animation.py
@@ -18,18 +18,24 @@ print(df_DA.tail(3))
 df_AA.Date = pd.to_datetime(df_AA.Date)
 df_DA.Date = pd.to_datetime(df_DA.Date)
 
+# Coronavirus Outbreak in the USA
+date_covid = datetime.datetime(2020, 2, 13)
+
 plt.rcParams["figure.figsize"] = [10, 5]
 plt.figure("American Airlines Stock vs Delta Airlines Stock")
 plt.xlabel("Date")
 plt.ylabel("Stock Price")
 
 # Animated Drawing
-for i in range(0, len(df_AA.Date), 10):
+for i in range(0, len(df_AA.Date), 9):
     plt.clf()
     plt.xlim(datetime.datetime(2007, 7, 2), datetime.datetime(2020, 3, 31))
     plt.ylim(0, 65)
     plt.plot(df_AA.Date[0:i], df_AA.Close[0:i], 'b', label="American Airlines")
     plt.plot(df_DA.Date[0:i], df_DA.Close[0:i], 'r', label="Delta Airlines")
+    if df_AA.Date[i] >= date_covid :
+        plt.plot(date_covid, df_AA.Close[df_AA.index[df_AA.Date == date_covid][0]], 'ko', label="Covid-19 Outbreak")
+        plt.plot(date_covid, df_DA.Close[df_DA.index[df_DA.Date == date_covid][0]], 'ko')
     plt.legend(loc="upper left")
     plt.draw()
     plt.pause(0.00001)
